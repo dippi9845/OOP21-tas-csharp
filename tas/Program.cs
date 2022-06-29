1a94678981d64a95b9a7b158c64aa32ed718d5cd
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-06-29 22:30:57 +0200

    init the project

---
+++ tas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
