a54e2db8f1dda465ba41928bac92e3cd5b7f7167
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-06-30 12:50:06 +0200

    created Enemy interface

---
+++ tas/Gabos/IEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tas.Gabos
+{
+    internal interface IEnemy : IEntity
+    {
+
+        void MoveForward();
+
+        void DealDamage(double damage);
+
+        bool IsDead();
+
+        double GetHealth();
+
+        int GetMoney();
+
+        int GetDamage();
+
+        bool IsPathCompleted();
+
+    }
+}
