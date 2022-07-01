7e4a24057d2e224b6fc2d3e55767a907f2398c59
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-01 21:29:33 +0200

    Update MultipleTower.cs
    created abstract multiple tower class

--- tas/Filippo Di Pietro/MultipleTower.cs
+++ tas/Filippo Di Pietro/MultipleTower.cs
@@ -1,8 +1,48 @@
-﻿
+﻿using System.Collections.Generic;
+using tas.Gabos;
 
 namespace tas.Filippo_Di_Pietro
 {
-    class MultipleTower
+    public abstract class AbstractMultipleTower : AbstractBasicTower
     {
+        private List<IEnemy> Targets { get; }
+
+        private int MaxTarget { get; }
+
+        public AbstractMultipleTower(Position pos, int damage, int radius, int delay, int cost, string towerName, IList<IEnemy> enemyList, int maxTarget) : base(pos, damage, radius, delay, cost, towerName, enemyList)
+        {
+            MaxTarget = maxTarget;
+            Targets = new List<IEnemy>();
+        }
+
+        protected bool Contains(IEnemy e) => Targets.Contains(e);
+
+        protected bool IsFull() => Targets.Count == MaxTarget;
+
+        protected void Remove(IEnemy e) => Targets.Remove(e);
+
+        protected void Clear() => Targets.Clear();
+
+        protected override void Attack()
+        {
+            foreach (var enemy in Targets)
+            {
+                enemy.DealDamage(Damage);
+            }
+        }
+
+        protected void AddTarget(IEnemy e) => Targets.Add(e);
+
+        protected abstract bool IsValidTarget(IEnemy e);
+
+    }
+
+    public class BasicMultipleTower : AbstractBasicTower
+    {
+        public override void Compute()
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
