1c5418ef49f5a8272431c8ee63037dd015d4845b
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-01 15:35:00 +0200

    Create Towers.cs

---
+++ tas/Filippo Di Pietro/Towers.cs
@@ -0,0 +1,39 @@
+﻿using tas.Gabos;
+using tas.Filippo_Di_Pietro.Tower;
+using System.Collections.Generic;
+using System;
+
+namespace tas.Filippo_Di_Pietro
+{
+    public sealed class Towers
+    {
+        private Towers() { }
+
+        public static bool IsInRange(Position x, Position y, int radius)
+        {
+            return Position.FindDistance(x, y) <= radius;
+        }
+
+        public static bool IsTargetInRange(ITower t, IEnemy e)
+        {
+            return IsInRange(t.Pos, e.Position, t.Radius);
+        }
+
+        public static IEnemy FindFisrtInRange(ITower t, IList<IEnemy> enemyList)
+        {
+            return FindFirstEnemyByPredicate(x => IsTargetInRange(t, x), enemyList);
+        }
+
+        public static IEnemy FindFirstEnemyByPredicate(Predicate<IEnemy> filter, IList<IEnemy> enemyList)
+        {
+            foreach (var enemy in enemyList)
+            {
+                if (filter.Invoke(enemy))
+                {
+                    return enemy;
+                }
+            }
+            return null;
+        }
+    }
+}
