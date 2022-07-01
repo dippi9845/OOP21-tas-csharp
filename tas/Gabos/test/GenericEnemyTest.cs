3c4a920f4692f280210a727bb589fc701e2dcf0a
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-07-01 18:36:14 +0200

    created first test setup

---
+++ tas/Gabos/test/GenericEnemyTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using tas.Gabos.utils;
+using tas.Gabos.enemy;
+
+namespace tas.Gabos.test
+{
+    [TestFixture]
+    public class GenericEnemyTest
+    {
+
+        [Test]
+        public void TestGetPosition()
+        {
+            Position pos = new Position(10, 10);
+            List<Position> lPos = new List<Position>();
+            lPos.Add(pos);
+            IEnemy enemy = new GenericEnemy(lPos, 1, 50, 10, 60, new Size(100, 100), "redEnemy");
+
+            Assert.AreEqual(pos.ToString(), enemy.Position.ToString());
+        }
+
+    }
+}
