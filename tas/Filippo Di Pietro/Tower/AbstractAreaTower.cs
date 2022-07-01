9a1c08d5cd06a61410968028b165add41702d533
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-01 22:29:40 +0200

    Create AbstractAreaTower.cs

---
+++ tas/Filippo Di Pietro/Tower/AbstractAreaTower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using tas.Gabos;
+using tas.Gabos.enemy;
+
+namespace tas.Filippo_Di_Pietro.Tower
+{
+    public abstract class AbstractAreaTower : AbstractMultipleTower
+    {
+        private int AttackRadius { get; }
+
+        public AbstractAreaTower(Position pos, int damage, int radius, int delay, int cost, string towerName, IList<IEnemy> enemyList, IList<IEnemy> enemyList1, int maxTarget, int attackRadius)
+            : base(pos, damage, radius, delay, cost, towerName, enemyList, enemyList, maxTarget)
+        {
+
+        }
+    }
+}
