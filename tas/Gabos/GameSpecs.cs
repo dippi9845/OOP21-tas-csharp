0e4e7679b4afeefb6e6dac6d2268528cf5abed61
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-07-01 17:04:01 +0200

    created partial GameSpecs

---
+++ tas/Gabos/GameSpecs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tas.Gabos
+{
+    internal class GameSpecs
+    {
+
+        private int _tickRate;
+
+        public GameSpecs()
+        {
+            this._tickRate = 60;
+        }
+
+        public int GetTickRate() => this._tickRate;
+
+
+    }
+}
