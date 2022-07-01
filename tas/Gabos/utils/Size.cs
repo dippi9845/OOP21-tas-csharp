d59165ac559cd851e7f301d1c977482724df9227
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-07-01 18:29:07 +0200

    added documentation

--- tas/Gabos/utils/Size.cs
+++ tas/Gabos/utils/Size.cs
@@ -1,17 +1,38 @@
 ﻿namespace tas.Gabos.utils
 {
+    
+    /// <summary>
+    ///     Class that handles a size.
+    /// </summary>
     public class Size
     {
+        
+        /// <summary>
+        ///     The width of the size.
+        /// </summary>
         public double Width { get; }
 
+        /// <summary>
+        ///     The Height of the size.
+        /// </summary>
         public double Height { get; }
 
+        /// <summary>
+        ///     Constructor that creates the size.
+        /// </summary>
+        /// <param name="width"> The width of the size.</param>
+        /// <param name="height"> The Height of the size.</param>
         public Size(double width, double height)
         {
             Width = width;
             Height = height;
         }
 
+        /// <summary>
+        ///     Constructor that creates the size.
+        /// </summary>
+        /// <param name="width"> The width of the size.</param>
+        /// <param name="height"> The Height of the size.</param>
         public Size(int width, int height)
         {
             Width = width;
