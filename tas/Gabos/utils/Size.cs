69e1546511b5c459458cc64079e53fc17f39c978
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-07-01 18:27:29 +0200

    created Size class

---
+++ tas/Gabos/utils/Size.cs
@@ -0,0 +1,22 @@
+﻿namespace tas.Gabos.utils
+{
+    public class Size
+    {
+        public double Width { get; }
+
+        public double Height { get; }
+
+        public Size(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public Size(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+    }
+}
