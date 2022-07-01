6c601421082d8e0e03a5becc450e7ebad9cc2c15
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-07-01 17:53:48 +0200

    added documentation

--- tas/Gabos/IEntity.cs
+++ tas/Gabos/IEntity.cs
@@ -7,12 +7,25 @@ using System.Threading.Tasks;
 namespace tas.Gabos
 {
     internal interface IEntity
+    /// <summary>
+    ///     An interface that models an Entity
+    /// </summary>
+    public interface IEntity
     {
 
+        /// <summary>
+        ///     The position of the entity.
+        /// </summary>
         Position Position { get; }
         
+        /// <summary>
+        ///     The body dimension of the entity.
+        /// </summary>
         Tuple<int, int>  BodyDimension { get; }
 
+        /// <summary>
+        ///     The name of the entity
+        /// </summary>
         string EntityName { get; }
 
     }
