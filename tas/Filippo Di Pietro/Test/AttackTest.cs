3ed85cb850f54ea9a70f35a3deed91a5a12f723f
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-03 00:57:53 +0200

    Update AttackTest.cs
    added skeletons

--- tas/Filippo Di Pietro/Test/AttackTest.cs
+++ tas/Filippo Di Pietro/Test/AttackTest.cs
@@ -1,8 +1,30 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
+using tas.Gabos.enemy;
 
 namespace tas.Filippo_Di_Pietro.Test
 {
-    class AttackTest
+    [TestFixture]
+    public class AttackTest
     {
+        [Test]
+        public void BasicTowerAttack()
+        {
+            IList<IEnemy> enemyList = new List<IEnemy>();
+            IEnemy e1 = new FakeEnemy
+
+        }
+
+        [Test]
+        public void MultipleTowerAttack()
+        {
+
+        }
+
+        [Test]
+        public void AreaTowerAttack()
+        {
+
+        }
     }
 }
