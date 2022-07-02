08c094b13b8a642dc2e178b7169531acf5561b74
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-07-02 14:39:02 +0200

    created first version of EnemyBuilder

--- tas/Gabos/enemy/EnemyBuilder.cs
+++ tas/Gabos/enemy/EnemyBuilder.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace tas.Gabos.enemy
 {
+
+    /// <summary>
+    ///     Class that implements an IEnemyBuilder.
+    /// </summary>
     public class EnemyBuilder : IEnemyBuilder
     {
+
+        private IList<Position> _nodesPosition;
+
+        /// <summary>
+        ///     Constructor that sets up the builder.
+        /// </summary>
+        /// <param name="nodesPosition"></param>
+        /// <exception cref="ArgumentException"> If the nodesPosition is empty.</exception>
+        public EnemyBuilder(IList<Position> nodesPosition)
+        {
+            if (nodesPosition.Count == 0)
+            {
+                throw new ArgumentException("nodesPosition is empty");
+            }
+
+            _nodesPosition = nodesPosition;
+        }
+
+        /// <summary>
+        ///     Generates an enemy by the given stats
+        /// </summary>
+        /// <returns> The requested enemy.</returns>
+        private IEnemy SpawnGenericEnemy()
+        {
+            // to be implemented
+            throw new NotImplementedException();
+        }
+
+        public IEnemy SpawnRedEnemy()
+        {
+            // to be implemented
+            throw new NotImplementedException();
+        }
+
+        public IEnemy SpawnGreenEnemy()
+        {
+            // to be implemented
+            throw new NotImplementedException();
+        }
+
+        public IEnemy SpawnPinkEnemy()
+        {
+            // to be implemented
+            throw new NotImplementedException();
+        }
+
     }
 }
