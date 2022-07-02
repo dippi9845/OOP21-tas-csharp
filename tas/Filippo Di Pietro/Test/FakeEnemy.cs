c62df4816e461f9d138d871af859444188d51395
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-03 00:56:02 +0200

    Create FakeEnemy.cs

---
+++ tas/Filippo Di Pietro/Test/FakeEnemy.cs
@@ -0,0 +1,42 @@
+﻿
+using tas.Gabos;
+using tas.Gabos.enemy;
+using tas.Gabos.utils;
+
+namespace tas.Filippo_Di_Pietro.Test
+{
+    public class FakeEnemy : IEnemy
+    {
+        public FakeEnemy(Position pos, double health)
+        {
+            Position = pos;
+            Health = health;
+            Money = 0;
+            Damage = 0;
+            BodyDimension = new Size(50, 50);
+            EntityName = "Fake enemy";
+        }
+        public double Health { get; private set; }
+
+        public int Money { get; }
+
+        public int Damage { get; }
+
+        public Position Position { get; }
+
+        public Size BodyDimension { get; }
+
+        public string EntityName { get; }
+
+        public void DealDamage(double damage) => Health -= damage;
+
+        public bool IsDead() => Health <= 0;
+
+        public bool IsPathCompleted() => false;
+
+        public void MoveForward()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
