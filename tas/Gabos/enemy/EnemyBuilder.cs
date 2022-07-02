853de0f829fa1d61342df9c7499ef6a173d588b2
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-07-02 14:29:56 +0200

    created the enemy builder interface

---
+++ tas/Gabos/enemy/EnemyBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace tas.Gabos.enemy
+{
+    internal interface IEnemyBuilder
+    {
+
+        /// <summary>
+		///		Returns a red enemy.
+		/// </summary>
+		/// <returns> The red enemy.</returns>
+		IEnemy SpawnRedEnemy();
+
+		/// <summary>
+		///		Returns a green enemy.
+		/// </summary>
+		/// <returns> The green enemy.</returns>
+		IEnemy SpawnGreenEnemy();
+
+		/// <summary>
+		///		Returns a pink enemy.
+		/// </summary>
+		/// <returns> The pink enemy.</returns>
+		IEnemy SpawnPinkEnemy();
+        
+	}
+}
