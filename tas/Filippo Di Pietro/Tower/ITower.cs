4692f5de93d60f359d0f49ec2dc2b313cb7546d4
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-03 13:41:44 +0200

    Update ITower.cs
    Added doc

--- tas/Filippo Di Pietro/Tower/ITower.cs
+++ tas/Filippo Di Pietro/Tower/ITower.cs
@@ -2,20 +2,44 @@
 
 namespace tas.Filippo_Di_Pietro
 {
+    /// <summary>
+    /// This interface model a generic tower on the map
+    /// </summary>
     public interface ITower : IEntity
     {
+        /// <summary>
+        /// Damage of the tower
+        /// </summary>
         public int Damage { get; }
 
+        /// <summary>
+        /// Radius of action of the tower
+        /// </summary>
         public int Radius { get; }
 
+        /// <summary>
+        /// Cost for buildingthat tower
+        /// </summary>
         public int Cost { get; }
 
+        /// <summary>
+        /// Delay of attack of the tower
+        /// </summary>
         public int Delay { get; }
 
+        /// <summary>
+        /// Name of the tower
+        /// </summary>
         public string TowerName { get; }
 
+        /// <summary>
+        /// Current position ofthe tower
+        /// </summary>
         public Position Pos { get; }
 
+        /// <summary>
+        /// Generic behavior of the tower
+        /// </summary>
         void Compute();
     }
 }
