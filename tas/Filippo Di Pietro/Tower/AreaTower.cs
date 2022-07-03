ca1ba03c6633ad907800532924ae793db2c0104c
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-03 16:51:02 +0200

    Added doc

--- tas/Filippo Di Pietro/Tower/AreaTower.cs
+++ tas/Filippo Di Pietro/Tower/AreaTower.cs
@@ -4,22 +4,55 @@ using tas.Gabos.enemy;
 
 namespace tas.Filippo_Di_Pietro.Tower
 {
-    public abstract class AreaTower : AbstractMultipleTower
+    /// <summary>
+    /// An abstract tower that need only a method to be instanced, FindFirstTarget() This class model a tower that one it found an enemy, all the other near will be attacked
+    /// </summary>
+    public abstract class AbstractAreaTower : AbstractMultipleTower
     {
+        /// <summary>
+        /// Radius of the attack
+        /// </summary>
         private int AttackRadius { get; }
 
+        /// <summary>
+        /// Position of the first target
+        /// </summary>
         protected Position TargetPosition { get; set; }
 
-        public AreaTower(Position pos, int damage, int radius, int delay, int cost, string towerName, IList<IEnemy> enemyList, int maxTarget, int attackRadius)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="pos">Position of the tower</param>
+        /// <param name="damage">Damage of the tower</param>
+        /// <param name="radius">Radius of the tower</param>
+        /// <param name="delay">Delay of the tower</param>
+        /// <param name="cost">Cost of the tower</param>
+        /// <param name="towerName">Tower Name of the tower</param>
+        /// <param name="enemyList">Current enemy present in the field</param>
+        /// <param name="maxTarget">Max number of target that this tower can handle at the time</param>
+        /// <param name="attackRadius">Range of attack given by the first target</param>
+        public AbstractAreaTower(Position pos, int damage, int radius, int delay, int cost, string towerName, IList<IEnemy> enemyList, int maxTarget, int attackRadius)
             : base(pos, damage, radius, delay, cost, towerName, enemyList, maxTarget)
         {
             AttackRadius = attackRadius;
         }
 
+        /// <summary>
+        /// Checks if the enemy e is near to the target choose by the tower
+        /// </summary>
+        /// <param name="e">enemy to be checked</param>
+        /// <returns>True if the enemy is near to the choosen target</returns>
         protected override bool IsValidTarget(IEnemy e) => Towers.IsInRange(e.Position, TargetPosition, AttackRadius);
 
+        /// <summary>
+        /// Abstract method, delegate to concrete class the decision to choose the first target
+        /// </summary>
+        /// <returns>null if no enemy was found, otherwise the actual enemy</returns>
         abstract protected IEnemy FindFirstTarget();
 
+        /// <summary>
+        /// Add all near enemy to the target
+        /// </summary>
         private void AddNearbyTarget()
         {
             IEnumerable<IEnemy> toAdd = Towers.FindAll(IsValidTarget, VisibleEnemyList);
@@ -32,6 +65,12 @@ namespace tas.Filippo_Di_Pietro.Tower
             }
         }
 
+        /// <summary>
+        /// Generic behavior:
+        /// Find fisrt enemy
+        /// Second, add all enemy in the nearby
+        /// Third, attack them
+        /// </summary>
         public override void Compute()
         {
             IEnemy target = FindFirstTarget();
@@ -45,13 +84,32 @@ namespace tas.Filippo_Di_Pietro.Tower
         }
     }
 
-    public class BasicAreaTower : AreaTower
+    /// <summary>
+    /// this calss model a simple area tower
+    /// </summary>
+    public class BasicAreaTower : AbstractAreaTower
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="pos">Position of the tower</param>
+        /// <param name="damage">Damage of the tower</param>
+        /// <param name="radius">Radius of the tower</param>
+        /// <param name="delay">Delay of the tower</param>
+        /// <param name="cost">Cost of the tower</param>
+        /// <param name="towerName">Tower Name of the tower</param>
+        /// <param name="enemyList">Current enemy present in the field</param>
+        /// <param name="maxTarget">Max number of target that this tower can handle at the time</param>
+        /// <param name="attackRadius">Range of attack given by the first target</param>
         public BasicAreaTower(Position pos, int damage, int radius, int delay, int cost, string towerName, IList<IEnemy> enemyList, int maxTarget, int attackRadius)
             : base(pos, damage, radius, delay, cost, towerName, enemyList, maxTarget, attackRadius)
         {
         }
 
+        /// <summary>
+        /// Find first enemy in range
+        /// </summary>
+        /// <returns>null if no enemy was found, otherwise the actual enemy</returns>
         protected override IEnemy FindFirstTarget() => Towers.FindFisrtInRange(this, VisibleEnemyList);
     }
 }
--- tas/Filippo Di Pietro/Tower/MultipleTower.cs
+++ tas/Filippo Di Pietro/Tower/MultipleTower.cs
@@ -4,12 +4,32 @@ using tas.Gabos.enemy;
 
 namespace tas.Filippo_Di_Pietro
 {
+    /// <summary>
+    /// An abstract class that model a Tower with an undefined number of enemies targeted.
+    /// </summary>
     public abstract class AbstractMultipleTower : AbstractBasicTower
     {
+        /// <summary>
+        /// List of Targets
+        /// </summary>
         protected List<IEnemy> Targets { get; }
 
+        /// <summary>
+        /// max number of possible target
+        /// </summary>
         protected int MaxTarget { get; }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="pos">Position of the tower</param>
+        /// <param name="damage">Damage of the tower</param>
+        /// <param name="radius">Radius of the tower</param>
+        /// <param name="delay">Delay of the tower</param>
+        /// <param name="cost">Cost of the tower</param>
+        /// <param name="towerName">Tower Name of the tower</param>
+        /// <param name="enemyList">Current enemy present in the field</param>
+        /// <param name="maxTarget">Max number of target that this tower can handle at the time</param>
         public AbstractMultipleTower(Position pos, int damage, int radius, int delay, int cost, string towerName, IList<IEnemy> enemyList, int maxTarget)
             : base(pos, damage, radius, delay, cost, towerName, enemyList)
         {
@@ -17,14 +37,33 @@ namespace tas.Filippo_Di_Pietro
             Targets = new List<IEnemy>();
         }
 
+        /// <summary>
+        /// Checks if an enemy is already targeted
+        /// </summary>
+        /// <param name="e">Enemy to be checked</param>
+        /// <returns>True if the enemy was already targeted, false otherwise</returns>
         protected bool Contains(IEnemy e) => Targets.Contains(e);
 
+        /// <summary>
+        /// Checks if the list of targets is full
+        /// </summary>
+        /// <returns>True if the list is full</returns>
         protected bool IsFull() => Targets.Count == MaxTarget;
 
+        /// <summary>
+        /// Remove an enemy from the list
+        /// </summary>
+        /// <param name="e">Enemy to be removed</param>
         protected void Remove(IEnemy e) => Targets.Remove(e);
 
+        /// <summary>
+        /// Clear the list of enemy
+        /// </summary>
         protected void Clear() => Targets.Clear();
 
+        /// <summary>
+        /// Attack all the targets
+        /// </summary>
         protected override void Attack()
         {
             foreach (var enemy in Targets)
@@ -33,27 +72,51 @@ namespace tas.Filippo_Di_Pietro
             }
         }
 
+        /// <summary>
+        /// Add target to the list
+        /// </summary>
+        /// <param name="e">enemy to be added</param>
         protected void AddTarget(IEnemy e) => Targets.Add(e);
 
+        /// <summary>
+        /// Checks if the enemy passed is valid to be added
+        /// </summary>
+        /// <param name="e">Enemy to be checked</param>
+        /// <returns>True if the enemy is valid to be added</returns>
         protected abstract bool IsValidTarget(IEnemy e);
 
     }
 
+    /// <summary>
+    /// This class is a basic implementation of AbstractMultipleTower, this BasicMultipleTower model a tower that attack the first maxTarget in the range
+    /// </summary>
     public class BasicMultipleTower : AbstractMultipleTower
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="pos">Position of the tower</param>
+        /// <param name="damage">Damage of the tower</param>
+        /// <param name="radius">Radius of the tower</param>
+        /// <param name="delay">Delay of the tower</param>
+        /// <param name="cost">Cost of the tower</param>
+        /// <param name="towerName">Tower Name of the tower</param>
+        /// <param name="enemyList">Current enemy present in the field</param>
+        /// <param name="maxTarget">Max number of target that this tower can handle at the time</param>
         public BasicMultipleTower(Position pos, int damage, int radius, int delay, int cost, string towerName, IList<IEnemy> enemyList, int maxTarget)
             : base(pos, damage, radius, delay, cost, towerName, enemyList, maxTarget)
         {
         }
-
+        /// <summary>
+        /// The genirc behavior of the tower is:
+        /// First remove all targets that are no more valid
+        /// Second find all target that are valid
+        /// Third attack
+        /// </summary>
         public override void Compute()
         {
-            IEnumerable<IEnemy> toRemove = Towers.FindAll(x => !Towers.IsTargetInRange(this, x) || x.IsDead(), Targets);
-            
-            foreach(IEnemy enemy in toRemove)
-            {
-                Remove(enemy);
-            }
+
+            Targets.RemoveAll(x => !Towers.IsTargetInRange(this, x) || x.IsDead());
 
             if (!IsFull())
             {
@@ -72,6 +135,11 @@ namespace tas.Filippo_Di_Pietro
 
         }
 
+        /// <summary>
+        /// Checks if the enemy passed is valid to be added, must be in range, targets list must be not full, the enemy must not be already in the list
+        /// </summary>
+        /// <param name="e">Enemy to be checked</param>
+        /// <returns>True if the enemy is valid to be added</returns>
         protected override bool IsValidTarget(IEnemy e) => Towers.IsTargetInRange(this, e) && !IsFull() && !Contains(e);
     }
 }
